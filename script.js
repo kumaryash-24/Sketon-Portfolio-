84e3d2ebd8f1f947fe6ca1a553466373d4189d6a
Kumar Yash <143955711+kumaryash-24@users.noreply.github.com>
2025-11-17 10:51:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
 pageTurnBtn.forEach((el, index) => {  
     el.onclick = () => {
-   
+     
         const pageTurnId = el.getAttribute('data-page');      
         const pageTurn = document.getElementById(pageTurnId);
                                                  
